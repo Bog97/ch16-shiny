b9d6ad64f12d90bc78e678b84a40df2c1b06cd79
Vineeth Santosh Varghese <vinvsv97@uw.edu>
2017-11-14 14:04:08 -0800

    editing

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,9 +1,9 @@
 # Exercise 1: Loading functions
 
 # Set your directory
-
+setwd("scripts/")
 
 # Source your BuildScatter.r script, exposing your BuildScatter function
-
+source('BuildScatter.r')
 
 # Use your BuildScatter function to draw a well labeled ggplot scatterplot of the iris data
